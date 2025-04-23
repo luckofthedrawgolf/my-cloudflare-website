701ca2aa852872e9f2d708d9544a7979d6eefe8f
luckofthedrawgolf <cstaus88@gmail.com>
2025-04-23 14:55:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const barIcon = document.querySelector('.hamburger .bar');
 const closeIcon = document.querySelector('.hamburger .close');

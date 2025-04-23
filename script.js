52febae0f21fbe67e898592b1d8b1701c0b4926c
luckofthedrawgolf <cstaus88@gmail.com>
2025-04-23 13:23:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active'); // This line toggles the color change!
 });

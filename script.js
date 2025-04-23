da0eb87df49e8108003f7fc034448291a1709f52
luckofthedrawgolf <cstaus88@gmail.com>
2025-04-23 15:35:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const hamburger = document.querySelector('#hamburger');
 const navLinks = document.querySelector('#navLinks');
+const barIcon = document.querySelector('.hamburger .bar');
+const closeIcon = document.querySelector('.hamburger .close');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
   hamburger.classList.toggle('active');
+
+  if (navLinks.classList.contains('active')) {
+    barIcon.style.display = 'none';        // Hide hamburger bars
+    closeIcon.style.display = 'block';     // Show X
+  } else {
+    barIcon.style.display = 'block';       // Show hamburger bars
+    closeIcon.style.display = 'none';      // Hide X
+  }
 });

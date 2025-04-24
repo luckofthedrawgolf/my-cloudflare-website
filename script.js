7653a674e8e35e60c42fe04d241f21fe9740a9ce
luckofthedrawgolf <cstaus88@gmail.com>
2025-04-24 10:02:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const hamburger = document.querySelector('.hamburger');  // <-- fixed this line!
+const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('#navLinks');
-const barIcon = document.querySelector('.hamburger .bar');
-const closeIcon = document.querySelector('.hamburger .close');
+const barIcon = document.querySelector('.bar');
+const closeIcon = document.querySelector('.close');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
   hamburger.classList.toggle('active');
 
-  if (navLinks.classList.contains('active')) {
+  if (hamburger.classList.contains('active')) {
     barIcon.style.display = 'none';        // Hide hamburger bars
     closeIcon.style.display = 'block';     // Show X icon
   } else {
     barIcon.style.display = 'block';       // Show hamburger bars
     closeIcon.style.display = 'none';      // Hide X icon
   }
+});

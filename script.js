773f64786eba87032c91cab504c08eeb5909d033
luckofthedrawgolf <cstaus88@gmail.com>
2025-04-25 11:03:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 window.onload = function() {
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const overlay = document.querySelector('.overlay');
 
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    hamburger.classList.toggle('active');
-  });
+    // Open drawer
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
+        overlay.classList.toggle('show'); // Show overlay when drawer is open
+    });
+
+    // Close drawer when clicking the overlay
+    overlay.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('active');
+        overlay.classList.remove('show');
+    });
 };
